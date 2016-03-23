9ce891247adb13b7c679407e1664858107bf432d
iamsueshi <susanwliu@gmail.com>
2016-03-22 22:16:43 -0700

    final-proj

--- final-project/myscripts.js
+++
@@ -1,3 +0,0 @@
-//Enter email in the subscribe form and a popup will show as "Added!"
-//Have a "Read More" and text will slide out
-//maybe change "Older Posts" to a dropdown menu that will change to the selected blog post
